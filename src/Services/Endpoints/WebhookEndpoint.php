2837540bf8df743eacbf9346be786add881797d0
Pedro Augusto <pams.pedro@gmail.com>
2021-04-26 21:06:39 -0300

    feat: cria operacoes para o webhook

---
+++ src/Services/Endpoints/WebhookEndpoint.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Accordous\AsaasClient\Services\Endpoints;
+
+class WebhookEndpoint extends Endpoint
+{
+    private const BASE_URI = '/webhook';
+
+    public function index()
+    {
+        return $this->client()->get(self::BASE_URI);
+    }
+
+    public function store(array $attributes)
+    {
+        return $this->client()->post(self::BASE_URI, $this->validate($attributes));
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'url' => 'required',
+            'email' => 'required',
+            'apiVersion' => 'required',
+            'enabled' => 'required',
+            'interrupted' => 'required',
+        ];
+    }
+
+    protected function messages(): array
+    {
+        return [
+            'url' => 'URL é obrigatória.',
+            'email' => 'Email é obrigatória.',
+            'apiVersion' => 'Versão utilizada da API. Utilize "3" para a versão v3.',
+            'enabled' => 'Habilitar ou não o webhook.',
+            'interrupted' => 'Situação da fila de sincronização é obrigatória.',
+        ];
+    }
+
+    protected function attributes(): array
+    {
+        return [
+            'url',
+            'email',
+            'apiVersion',
+            'enabled',
+            'interrupted',
+            'authToken',
+        ];
+    }
+}
