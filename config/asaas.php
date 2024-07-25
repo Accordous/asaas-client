50890832f381adb733351106a8b2748bde714826
Lúdio Oliveira <ludio.ao@gmail.com>
2024-07-24 21:43:52 -0400

    fix: default asaas api url

--- config/asaas.php
+++ config/asaas.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => env('ASAAS_HOST', 'https://www.asaas.com'),
-    'api' => env('ASAAS_API', '/api/v3'),
+    'host' => env('ASAAS_HOST', 'https://api.asaas.com'),
+    'api' => env('ASAAS_API', '/v3'),
     'token' => env('ASAAS_TOKEN', ''),
 ];
