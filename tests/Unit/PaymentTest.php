4fd5b97c103c6b2c64112234c6aace196cad97b2
Rafael Iga <rafael.f.iga@gmail.com>
2025-04-10 12:34:29 -0300

    test: payment with credit card params

--- tests/Unit/PaymentTest.php
+++ tests/Unit/PaymentTest.php
@@ -64,4 +64,105 @@ class PaymentTest extends TestCase
 
         $this->assertEquals('200', $removed->getStatusCode());
     }
+
+    /**
+     * @test
+     */
+    public function canCreatePaymentWithCreditCardToken()
+    {
+        $service = new AsaasService(Config::get('asaas.token'));
+
+        $creditCardNumber = '4444444444444444';
+        $date = new \DateTime();
+        $date->modify('+1 year');
+
+        $customerId = $service->customers()->store([
+            'name' => $this->faker->name,
+            'cpfCnpj' => '711.168.120-70', // fake valid cpf
+            'postalCode' => '01001001',
+            'email' => $this->faker->email
+        ])->json('id');
+
+        $request = $service->creditCards()->tokenize([
+            'customer' => $customerId,
+            'creditCard' => [
+                'holderName' => $this->faker->name,
+                'number' => $creditCardNumber,
+                'expiryMonth' => $date->format('m'),
+                'expiryYear' => $date->format('Y'),
+                'ccv' => $this->faker->numerify('###'),
+            ],
+            'creditCardHolderInfo' => [
+                'name' => $this->faker->name,
+                'email' => $this->faker->email,
+                'cpfCnpj' => '711.168.120-70',
+                'postalCode' => '01001-001',
+                'addressNumber' => $this->faker->numerify('###'),
+                'phone' => $this->faker->phoneNumber,
+                'mobilePhone' => $this->faker->phoneNumber,
+            ],
+        ]);
+
+        $token = $request->json()['creditCardToken'];
+
+        $payment = $service->payments()->store([
+            'customer' => $customerId,
+            'billingType' => 'CREDIT_CARD',
+            'value' => 5,
+            'dueDate' => now(),
+            'creditCardToken' => $token,
+            'remoteIp' => '123.123.123.123'
+        ]);
+
+        $service->payments()->destroy($payment['id']);
+
+        $this->assertEquals('payment', $payment['object']);
+    }
+
+    /**
+     * @test
+     */
+    public function canCreatePaymentWithCreditCardInfo()
+    {
+        $service = new AsaasService(Config::get('asaas.token'));
+
+        $creditCardNumber = '4444444444444444';
+        $date = new \DateTime();
+        $date->modify('+1 year');
+
+        $customerId = $service->customers()->store([
+            'name' => $this->faker->name,
+            'cpfCnpj' => '711.168.120-70', // fake valid cpf
+            'postalCode' => '01001001',
+            'email' => $this->faker->email
+        ])->json('id');
+
+        $payment = $service->payments()->store([
+            'customer' => $customerId,
+            'billingType' => 'CREDIT_CARD',
+            'value' => 5,
+            'dueDate' => now(),
+            'creditCard' => [
+                'holderName' => $this->faker->name,
+                'number' => $creditCardNumber,
+                'expiryMonth' => $date->format('m'),
+                'expiryYear' => $date->format('Y'),
+                'ccv' => $this->faker->numerify('###'),
+            ],
+            'creditCardHolderInfo' => [
+                'name' => $this->faker->name,
+                'email' => $this->faker->email,
+                'cpfCnpj' => '711.168.120-70',
+                'postalCode' => '01001-001',
+                'addressNumber' => $this->faker->numerify('###'),
+                'phone' => $this->faker->phoneNumber,
+                'mobilePhone' => $this->faker->phoneNumber,
+            ],
+            'remoteIp' => '123.123.123.123'
+        ]);
+
+        $service->payments()->destroy($payment['id']);
+
+        $this->assertEquals('payment', $payment['object']);
+    }
 }
